98880639e25d73a2c4031acd570d3687ecb71b20
esp32-urobot <75105147+urobot2011@users.noreply.github.com>
2022-04-08 20:28:37 +0900

    Add files via upload

--- src/drmdl_js.js
+++ src/drmdl_js.js
@@ -1,5 +1,6 @@
 //drmdl.js v1.0.0
 var drmdl_js_v = '1.0.0';
+var drmdl_plugin_v = [];
 function drmdl_js(ioc) {
     this.ioc = ioc;
 	this.ioce = document.querySelectorAll(ioc);
@@ -136,13 +137,13 @@ drmdl_js.prototype.ajax = function(src) {
 	return loadDoc(src);
 };
 
-drmdl_js.prototype.plugin_add = function(src) {
-	/*_$('head').appendTo(`
+/*drmdl_js.prototype.plugin_add = function(src) {
+	var error_Fff_a_s_d_1 = `\/*_$('head').appendTo(`
     <script src="`+src+`"></script>
     `);
 	_$('body').prepend(`
     <script src="`+src+`"></script>
-    `);*/
+    `);*\/`;
 	function loadDoc(src) {
 		const xhttp = new XMLHttpRequest();
 		xhttp.onload = function() {
@@ -156,16 +157,89 @@ drmdl_js.prototype.plugin_add = function(src) {
 	`+loadDoc(src)+`
 	</script>`;
 	_$('body').appendTo(script);
+};*/
+
+drmdl_js.prototype.plugin_add = function(src) {
+	_$('body').appendTo(`
+	<script>`
+		+script+`
+	</script>`);
+};
+
+/*drmdl_js.prototype.addClass = function(...name) {
+	if(name.length > 0){
+		var aaaa = name[0];
+		for (var i = 1; i < name.length; i++) {
+    		aaaa += " "+name[i];
+		}
+		this.ioce.classList.add(aaaa);
+	}else{
+		this.ioce.classList.add(name);
+	}
+};*/
+
+drmdl_js.prototype.addClass = function(...name) {
+	if(name.length > 0){
+		var aaaa = "";
+		for (var i = 0; i < name.length; i++) {
+    		aaaa += " "+name[i];
+		}
+		for (let elem of this.ioce) {
+			elem.className += aaaa;
+		}
+	}else{
+		for (let elem of this.ioce) {
+			elem.className += name;
+		}
+	}
+};
+
+drmdl_js.prototype.removeClass = function(...name) {
+	if(name.length > 0){
+		for (let elem of this.ioce) {
+			for (var i = 0; i < name.length; i++) {
+    			elem.classList.remove(name[i]);
+			}
+		}
+	}else{
+		for (let elem of this.ioce) {
+			elem.classList.remove(name);
+		}
+	}
+};
+
+drmdl_js.prototype.toggleClass = function(...name) {
+	if(name.length > 0){
+		for (let elem of this.ioce) {
+			for (var i = 0; i < name.length; i++) {
+				elem.classList.toggle(name[i]);
+			}
+		}
+	}else{
+		for (let elem of this.ioce) {
+			elem.classList.toggle(name);
+		}
+	}
+};
+
+drmdl_js.prototype.AlternateToggleClass = function(...name) {
+	if(name.length > 0){
+		for (let elem of this.ioce) {
+			for (var i = 0; i < name.length-1; i++) {
+				elem.classList.toggle(name[i]);
+				elem.classList.toggle(name[i]);
+				elem.classList.toggle(name[i+1]);
+			}
+		}
+	}else{
+		for (let elem of this.ioce) {
+			elem.classList.toggle(name);
+		}
+	}
 };
 
 function _$(name){
 	return new drmdl_js(name);
-}
-
-function $$$(){
-	function $(name){
-		return new drmdl_js(name);
-	}
 }
 // replace jquery
 // no jquery crash
